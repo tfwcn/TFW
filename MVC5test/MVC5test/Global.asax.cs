0c7d266e8bb43f372a25d2bfda183c32568a0ab2
tfwcn <tfwcn@sina.cn>
2014-09-05 14:11:13 +0800

    test

--- MVC5test/MVC5test/Global.asax.cs
+++ MVC5test/MVC5test/Global.asax.cs
@@ -11,8 +11,8 @@ namespace MVC5test
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();//1
+            RouteConfig.RegisterRoutes(RouteTable.Routes);//2
         }
     }
 }
