669ce484b6de702607477e737fa02eabf4b0c4cc
tfwcn <tfwcn@sina.cn>
2014-09-09 22:35:39 +0800

    test

---
+++ MVC5test/DBHelper/PostgresHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using Npgsql;
+
+namespace DBHelper
+{
+    public class PostgresHelper
+    {
+        private DbConnection GetConnection()
+        {
+            DbConnection conn = null;
+            return conn;
+        }
+        public DbDataReader GetDataReader()
+        {
+            NpgsqlConnection conn = GetConnection() as NpgsqlConnection;
+            conn.Open();
+            DbCommand com = new NpgsqlCommand("",conn);
+            DbDataReader dr = com.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            return dr;
+        }
+    }
+}
