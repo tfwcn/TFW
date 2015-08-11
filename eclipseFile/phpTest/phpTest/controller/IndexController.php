cf70fd81c50261e5bf692ccd78efa1ebe659d898
tfwcn <tfwcn@sina.cn>
2015-08-11 17:03:08 +0800

    test

--- eclipseFile/phpTest/phpTest/controller/IndexController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace phpTest\controller{
-	class IndexController extends Controller{
-		public function AView(){
-			return $this->View();
-		}
-	}
-}
--- eclipseFile/phpTest/phpTest/model/M1.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class M1 {
-	public $P1;
-	public function M1() {
-		$this->P1 = 200;
-		echo "M1 class";
-	}
-}
-?>
